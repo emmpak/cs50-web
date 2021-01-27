72af87bfb193d77f9dd5c54481c18f919b3170be
emmpak <eiliev05@yahoo.com>
2021-01-27 09:48:40 +0200

    Add wiki app_name

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wiki'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("<str:title>", views.entry, name="entry")
 ]
