2e4117b99747cb918451c02c8c900269a2abe238
emmpak <eiliev05@yahoo.com>
2021-03-10 18:19:52 +0000

    Commerce: Add Listing, Bid and Comment models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,35 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=255)
+    category = models.CharField(max_length=10)
+    image_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} created in {self.category} category by {self.user}"
+
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    currency = models.CharField(max_length=3)
+    cents = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.cents} {self.currency} for {self.listing}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user} commented on {self.listing}: {self.body}"
