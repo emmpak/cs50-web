685bb4c5280ff9b3fe531a2baffc6cd65e211789
emmpak <eiliev05@yahoo.com>
2021-03-11 09:41:42 +0000

    Commerce: Add listing form

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,38 +1,44 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+CURRENCIES = [ 
+  ('USD', 'USD'), 
+  ('EUR', 'EUR')
+]
 
 class User(AbstractUser):
-    pass
+  pass
 
 class Listing(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
-    title = models.CharField(max_length=64)
-    description = models.CharField(max_length=255)
-    category = models.CharField(max_length=10)
-    image_url = models.URLField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+  title = models.CharField(max_length=64)
+  description = models.TextField()
+  category = models.CharField(max_length=10)
+  image_url = models.URLField(blank=True)
+  cents = models.DecimalField(max_digits=9, decimal_places=2)
+  currency = models.CharField(max_length=3, choices=CURRENCIES)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.title} created in {self.category} category by {self.user}"
+  def __str__(self):
+    return f"{self.title} created in {self.category} category by {self.user} for {self.cents}"
 
 class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
-    currency = models.CharField(max_length=3)
-    cents = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+  listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+  cents = models.IntegerField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.cents} {self.currency} for {self.listing}"
+  def __str__(self):
+    return f"{self.cents} {self.currency} for {self.listing}"
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+  listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+  body = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.user} commented on {self.listing}: {self.body}"
+  def __str__(self):
+    return f"{self.user} commented on {self.listing}: {self.body}"
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -3,61 +3,77 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.forms import ModelForm, DecimalField 
 
-from .models import User
+from .models import User, Listing
+
+class ListingForm(ModelForm):
+  class Meta:
+    model = Listing
+    fields = ['title', 'description', 'category', 'image_url', 'cents', 'currency']
+    field_classes = { 'cents': DecimalField }
+    labels = { 'cents': 'Price' }
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+  return render(request, "auctions/index.html")
+
+def add(request):
+  if request.method == "POST":
+    return
+  else:
+    return render(request, "auctions/add_listing.html", {
+      "form": ListingForm()
+    })
 
 
 def login_view(request):
-    if request.method == "POST":
+  if request.method == "POST":
 
-        # Attempt to sign user in
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+    # Attempt to sign user in
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
 
-        # Check if authentication successful
-        if user is not None:
-            login(request, user)
-            return HttpResponseRedirect(reverse("index"))
-        else:
-            return render(request, "auctions/login.html", {
-                "message": "Invalid username and/or password."
-            })
+    # Check if authentication successful
+    if user is not None:
+      login(request, user)
+      return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "auctions/login.html")
+      return render(request, "auctions/login.html", {
+        "message": "Invalid username and/or password."
+      })
+  else:
+    return render(request, "auctions/login.html")
 
 
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse("index"))
+  logout(request)
+  return HttpResponseRedirect(reverse("index"))
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
+  if request.method == "POST":
+    username = request.POST["username"]
+    email = request.POST["email"]
 
-        # Ensure password matches confirmation
-        password = request.POST["password"]
-        confirmation = request.POST["confirmation"]
-        if password != confirmation:
-            return render(request, "auctions/register.html", {
-                "message": "Passwords must match."
-            })
+    # Ensure password matches confirmation
+    password = request.POST["password"]
+    confirmation = request.POST["confirmation"]
+    if password != confirmation:
+      return render(request, "auctions/register.html", {
+        "message": "Passwords must match."
+      })
 
-        # Attempt to create new user
-        try:
-            user = User.objects.create_user(username, email, password)
-            user.save()
-        except IntegrityError:
-            return render(request, "auctions/register.html", {
-                "message": "Username already taken."
-            })
-        login(request, user)
-        return HttpResponseRedirect(reverse("index"))
-    else:
-        return render(request, "auctions/register.html")
+    # Attempt to create new user
+    try:
+      user = User.objects.create_user(username, email, password)
+      user.save()
+    except IntegrityError:
+      return render(request, "auctions/register.html", {
+        "message": "Username already taken."
+      })
+    login(request, user)
+    return HttpResponseRedirect(reverse("index"))
+  else:
+    return render(request, "auctions/register.html")
